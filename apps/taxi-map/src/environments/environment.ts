a15caa4f8f7e134e1fe5cc0830650fa3a3821926
NoseferatuWKF <wkf2584@gmail.com>
2021-08-06 00:42:38 +0800

    splyt assessment init

--- apps/taxi-map/src/environments/environment.ts
+++ apps/taxi-map/src/environments/environment.ts
@@ -3,4 +3,14 @@
 
 export const environment = {
   production: false,
+  sgOffice: {
+    name: 'Singapore Office',
+    latitude: 1.285194,
+    longitude:  103.8522982
+  },
+  londonOffice: {
+    name: 'London Office',
+    latitude: 51.5049375,
+    longitude: -0.0964509
+  }
 };
--- apps/taxi-map/src/main.tsx
+++ apps/taxi-map/src/main.tsx
@@ -3,6 +3,8 @@ import * as ReactDOM from 'react-dom';
 
 import App from './app/app';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 ReactDOM.render(
   <StrictMode>
     <App />
